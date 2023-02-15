b9872cc1d2c9458dbf45baacc45b06d660a54738
Yernar Smagulov <yernar.smagulov02@gmail.com>
2023-02-15 01:33:08 -0800

    adding isIsomorphic.py

---
+++ isIsomorphic.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+                
+        s_dict = {}
+        t_dict = {}
+        
+        for i in range(len(s)):
+            if s[i] not in s_dict and t[i] not in t_dict:
+                s_dict[s[i]] = t[i]
+                t_dict[t[i]] = s[i]
+            elif s_dict.get(s[i]) != t[i] or t_dict.get(t[i]) != s[i]:
+                return False
+        
+        return True
